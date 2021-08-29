e25e76a10dca0333149e587b0f03268874f6a107
killbizz <45452791+killbizz@users.noreply.github.com>
2021-08-29 13:41:33 +0200

    added waveshaper TransferFunctionGraph component

--- Source/TransferFunctionGraphComponent.cpp
+++ Source/TransferFunctionGraphComponent.cpp
@@ -9,3 +9,124 @@
 */
 
 #include "TransferFunctionGraphComponent.h"
+#include "PluginProcessor.h"
+
+TransferFunctionGraphComponent::TransferFunctionGraphComponent(BiztortionAudioProcessor& p)
+	: audioProcessor(p)
+{
+	const auto& params = audioProcessor.getParameters();
+	for (auto param : params) {
+		if (param->getLabel() == juce::String("Waveshaper")) {
+			param->addListener(this);
+			// std::cout << param->getName(100);
+		}
+	}
+	startTimerHz(60);
+}
+
+TransferFunctionGraphComponent::~TransferFunctionGraphComponent()
+{
+	const auto& params = audioProcessor.getParameters();
+	for (auto param : params) {
+		if (param->getLabel() == juce::String("Waveshaper")) {
+			param->removeListener(this);
+		}
+	}
+}
+
+void TransferFunctionGraphComponent::parameterValueChanged(int parameterIndex, float newValue)
+{
+	parameterChanged.set(true);
+}
+
+void TransferFunctionGraphComponent::timerCallback()
+{
+	if (parameterChanged.compareAndSetBool(false, true)) {
+		updateParams();
+	}
+	repaint();
+}
+
+void TransferFunctionGraphComponent::paint(juce::Graphics& g)
+{
+	auto width = static_cast<float> (getWidth());
+	auto height = static_cast<float> (getHeight());
+
+	g.fillAll(juce::Colours::darkslategrey.withMultipliedBrightness(.4f));
+
+	// X
+	juce::Array<float> x;
+	int resolution = 800;
+
+	float min = -2;
+	float max = +2;
+	float range = max - min;
+	float delta = range / resolution;
+
+	float v = min;
+	for (int i = 0; i < resolution; i++)
+	{
+		x.add(v);
+		v = v + delta;
+	}
+
+	// Y
+	juce::Array<float> y;
+
+	for (int i = 0; i < resolution; i++)
+	{
+		float vNorm;
+		float v1 = tanhAmplitude * std::tanh(x[i] * tanhSlope) + sineAmplitude * std::sin(x[i] * sineFrequency);
+
+		// clip to -1...1
+		if (v1 <= -1)
+			vNorm = -1;
+		else if (v1 >= 1)
+			vNorm = 1;
+		else
+			vNorm = v1;
+		y.add(vNorm);
+	}
+
+	juce::Path t;
+
+	t.startNewSubPath(x[0], y[0]);
+	for (int i = 1; i < resolution; i++)
+		t.lineTo(x[i], y[i]);
+
+	t.applyTransform(juce::AffineTransform::scale(80.0f, 80.0f));
+	t.applyTransform(juce::AffineTransform::translation(width / 2.f, height / 2.f));
+	t.applyTransform(juce::AffineTransform::verticalFlip(height));
+
+	g.setColour(juce::Colours::white);
+	g.strokePath(t, juce::PathStrokeType(1.0f));
+}
+
+void TransferFunctionGraphComponent::updateParams()
+{
+	WaveshaperSettings settings = WaveshaperModule::getSettings(audioProcessor.apvts);
+	setTanhAmp(settings.tanhAmp);
+	setTanhSlope(settings.tanhSlope);
+	setSineAmp(settings.sinAmp);
+	setSineFreq(settings.sinFreq);
+}
+
+void TransferFunctionGraphComponent::setTanhAmp(float v)
+{
+	tanhAmplitude = v / 100.f;
+}
+
+void TransferFunctionGraphComponent::setTanhSlope(float v)
+{
+	tanhSlope = v;
+}
+
+void TransferFunctionGraphComponent::setSineAmp(float v)
+{
+	sineAmplitude = v / 100.f;
+}
+
+void TransferFunctionGraphComponent::setSineFreq(float v)
+{
+	sineFrequency = v;
+}
--- Source/TransferFunctionGraphComponent.h
+++ Source/TransferFunctionGraphComponent.h
@@ -9,3 +9,72 @@
 */
 
 #pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+class BiztortionAudioProcessor;
+
+class TransferFunctionGraphComponent : public juce::Component,
+	juce::AudioProcessorParameter::Listener,
+	juce::Timer
+{
+public:
+	//==============================================================================
+	TransferFunctionGraphComponent(BiztortionAudioProcessor&);
+	~TransferFunctionGraphComponent();
+
+	/** Receives a callback when a parameter has been changed.
+
+			IMPORTANT NOTE: This will be called synchronously when a parameter changes, and
+			many audio processors will change their parameter during their audio callback.
+			This means that not only has your handler code got to be completely thread-safe,
+			but it's also got to be VERY fast, and avoid blocking. If you need to handle
+			this event on your message thread, use this callback to trigger an AsyncUpdater
+			or ChangeBroadcaster which you can respond to on the message thread.
+		*/
+	void parameterValueChanged(int parameterIndex, float newValue) override;
+
+	/** Indicates that a parameter change gesture has started.
+
+		E.g. if the user is dragging a slider, this would be called with gestureIsStarting
+		being true when they first press the mouse button, and it will be called again with
+		gestureIsStarting being false when they release it.
+
+		IMPORTANT NOTE: This will be called synchronously, and many audio processors will
+		call it during their audio callback. This means that not only has your handler code
+		got to be completely thread-safe, but it's also got to be VERY fast, and avoid
+		blocking. If you need to handle this event on your message thread, use this callback
+		to trigger an AsyncUpdater or ChangeBroadcaster which you can respond to later on the
+		message thread.
+	*/
+	void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override { }
+	/** The user-defined callback routine that actually gets called periodically.
+
+		It's perfectly ok to call startTimer() or stopTimer() from within this
+		callback to change the subsequent intervals.
+	*/
+	void timerCallback() override;
+
+	void paint(juce::Graphics& g) override;
+
+	void resized() override
+	{
+		// This is called when the MainContentComponent is resized.
+		// If you add any child components, this is where you should
+		// update their positions.
+	}
+
+private:
+	// params
+	float tanhAmplitude, tanhSlope, sineAmplitude, sineFrequency;
+	BiztortionAudioProcessor& audioProcessor;
+	juce::Atomic<bool> parameterChanged{ false };
+
+	void updateParams();
+	void setTanhAmp(float v);
+	void setTanhSlope(float v);
+	void setSineAmp(float v);
+	void setSineFreq(float v);
+
+	//==============================================================================
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(TransferFunctionGraphComponent)
+};
