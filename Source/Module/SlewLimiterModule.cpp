ae392e8d114229603e88ec4bc28eb040c0e985bf
killbizz <45452791+killbizz@users.noreply.github.com>
2021-10-27 12:33:09 +0200

    added comments to SlewLimiter algorithm

--- Source/Module/SlewLimiterModule.cpp
+++ Source/Module/SlewLimiterModule.cpp
@@ -94,6 +94,10 @@ void SlewLimiterModuleDSP::processBlock(juce::AudioBuffer<float>& buffer, juce::
         float slewRise = slewMax * Ts * std::pow(slewMin / slewMax, rise.getNextValue());
         float slewFall = slewMax * Ts * std::pow(slewMin / slewMax, fall.getNextValue());
 
+        // trying to compute slewRise/Fall values with different functions (exponential is the best)
+        //float slewRise = 0.000001f / rise.getNextValue();
+        //float slewFall = 0.000001f / fall.getNextValue();
+
         // temporary variable to handle the last output at the end of the processing
         float temp = lastOutput;
 
